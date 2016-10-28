37aa3b87641af30a567ee180b83188cf31637a9a
Subhrajit Debnath <sdebnath@jahnelgroup.com>
2016-10-28 19:22:09 -0400

    Added spring initializer project

---
+++ src/main/java/com/team7/CmtApplication.java
@@ -0,0 +1,12 @@
+package com.team7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmtApplication.class, args);
+	}
+}
