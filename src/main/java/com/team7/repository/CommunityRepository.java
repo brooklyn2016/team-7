08c7dba2b5b3c17ab372251f71c3667989847d3f
Unknown <jonathantbeckman@gmail.com>
2016-10-29 05:26:53 -0400

    Added queryMethod for Communities, need to test!

--- src/main/java/com/team7/repository/CommunityRepository.java
+++ src/main/java/com/team7/repository/CommunityRepository.java
@@ -1,13 +1,20 @@
 package com.team7.repository;
 
 import com.team7.model.Community;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by jbeckman on 10/29/16.
  */
 @Repository
 public interface CommunityRepository extends PagingAndSortingRepository<Community, String> {
-    Community findOneByRegionAndCountry(String region, String country);
+    public Community findOneByRegionAndCountry(String region, String country);
+
+    @Query("select c from Community c where LOWER(TRIM(c.name)) like %:queryString% OR LOWER(TRIM(c.country)) like %:queryString% OR LOWER(TRIM(c.region)) like %:queryString%")
+    public List<Community> findByQuery(@Param("queryString") String queryString);
 }
