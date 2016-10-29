56870bffa20e5ca1e936d3d9cdd4af8cadf9330b
Subhrajit Debnath <sdebnath@jahnelgroup.com>
2016-10-28 20:18:58 -0400

    Added basic rest controller to test

--- src/main/java/com/team7/TestController.java
+++ src/main/java/com/team7/TestController.java
@@ -1,7 +1,11 @@
 package com.team7;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,5 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Component
 @RestController
 public class TestController {
+
+
+    @RequestMapping(value="/test", method= RequestMethod.GET)
+    ResponseEntity<String> testMessage(){
+        return new ResponseEntity<String>("Hello World", HttpStatus.OK);
+    }
 
 }
