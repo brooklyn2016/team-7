96d3b2c71d2e508e9dcf7419e66512a750d55b55
Katherine Pham <kpham39@gatech.edu>
2016-10-29 09:38:12 -0400

    put svg inside div

--- src/main/resources/static/js/pie.js
+++ src/main/resources/static/js/pie.js
@@ -7,103 +7,160 @@
     var update1 = [];
     var path;
 
+    var questions = [];
+
     var m = 100,
         r = 300,
         z = d3.scale.category20c();
 
     var JSON = {
-                "senegal": {
-                    "q1": "90",
-                    "q2": "20",
-                    "q3": "30",
-                    "q4": "70",
-                    "q5": "70"
-                },
-                "india": {
-                    "q1": "91",
-                    "q2": "20",
-                    "q3": "30",
-                    "q4": "71",
-                    "q5": "70"
-                },
-                "Drc": {
-                    "q1": "92",
-                    "q2": "20",
-                    "q3": "30",
-                    "q4": "72",
-                    "q5": "70"
-                }
-            };
+         "Haiti": [
+           {
+             "pk_statistics": null,
+             "stat": "Slaves freed",
+             "val": 12,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Communities receiving support",
+             "val": 30,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Community Members educated on rights and risks",
+             "val": 50,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Communities that have reached full maturity",
+             "val": 20,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Slaves effectively re-intergrated",
+             "val": 20,
+             "stat-id": null
+           }
+         ],
+         "Ghana": [
+           {
+             "pk_statistics": null,
+             "stat": "Slaves freed",
+             "val": 125,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Communities receiving support",
+             "val": 30,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Community Members educated on rights and risks",
+             "val": 500,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Communities that have reached full maturity",
+             "val": 200,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Slaves effectively re-intergrated",
+             "val": 20,
+             "stat-id": null
+           }
+         ],
+         "India": [
+           {
+             "pk_statistics": null,
+             "stat": "Slaves freed",
+             "val": 125,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Communities receiving support",
+             "val": 30,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Community Members educated on rights and risks",
+             "val": 500,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Communities that have reached full maturity",
+             "val": 200,
+             "stat-id": null
+           },
+           {
+             "pk_statistics": null,
+             "stat": "Slaves effectively re-intergrated",
+             "val": 20,
+             "stat-id": null
+           }
+         ]
+        }
 
+    var q0 = [];
     var q1 = [];
     var q2 = [];
     var q3 = [];
     var q4 = [];
-    var q5 = [];
 
     var i=0;
     $.each(JSON, function(n, item) {
 
-      console.log("ITEM: " + item);
-       $.each(item, function(question, value) {
-          console.log(question);
-          // console.log(value);
-          if (question == "q1") {
-            q1[i] = value;
-          } else if (question == "q2") {
-            q2[i] = value;
-          } else if (question == "q3") {
-            q3[i] = value;
-          } else if (question == "q4") {
-            q4[i] = value;
-          } else if (question == "q5") {
-            q5[i] = value;
+      // console.log("ITEM: " + item);
+       $.each(item, function(question, obj) {
+          // console.log(question);
+          // console.log(obj.val);
+          if (obj.stat == "Slaves freed") {
+            // console.log("yeah");
+            questions[0] = obj.stat;
+            q0[i] = obj.val;
+          } else if (obj.stat == "Communities receiving support") {
+            questions[1] = obj.stat;
+            q1[i] = obj.val;
+          } else if (obj.stat == "Community Members educated on rights and risks") {
+            questions[2] = obj.stat;
+            q2[i] = obj.val;
+          } else if (obj.stat == "Communities that have reached full maturity") {
+            questions[3] = obj.stat;
+            q3[i] = obj.val;
+          } else if (obj.stat == "Slaves effectively re-intergrated") {
+            questions[4] = obj.stat;
+            q4[i] = obj.val;
           }
        });
        i++;
     });
+    q0 = [q0];
     q1 = [q1];
     q2 = [q2];
     q3 = [q3];
     q4 = [q4];
-    q5 = [q5];
-
-    console.log(q1);
-    console.log(q2);
-    console.log(q3);
-    console.log(q4);
-    console.log(q5);
 
 
-    // d3.csv("2015-Indicators-All-Countries.csv", function(error, data) {
-    //   data.forEach(function(d, i) {
 
-    //     //convert object to array
-    //     var arr = Object.keys(d).map(function (key) { return d[key]; });
-    //     d = arr;
-    //     data[i] = arr;
-
-
-    //     if (tar === d3.select("#questions").node().value) {
-    //         array = [d.slice(0, d.length)];
-    //     }
-
-    //   });
-
-    //    for (var i = 0; i < data.length; i++) {
-    //         update1[i] = data[i].slice(0, data[i].length);
-    //     }
-
-    //    array[0][0] = 0; //so i don't have to make Target 0;
+    array = q0;
 
   
     // Insert an svg element (with margin) for each row in our dataset. A child g
     // element translates the origin to the pie center.
-    var area =d3.select(document.getElementById("pie_div"));
 
-    area.each(function (data) {
-      svg = d3.select("body").selectAll("svg")
-          .data(q1)
+      svg = d3.select("#pie_div").selectAll("svg")
+          .data(array)
         .enter().append("svg")
           .attr("width", (r + m) * 2)
           .attr("height", (r + m) * 2)
@@ -135,28 +192,29 @@
             .style("fill", function(d){ return "white"; })
 
 
-  });
+  // });
 
 
 
 
  function filter_selection() {
     var q = d3.select("#questions").node().value;
-    // console.log(q);
-
-    update1.forEach(function(d, i) {
-        // console.log(d[0]);
-        if (d[0] === d3.select("#questions").node().value) {
-            console.log("YEP");
-            array = [d.slice(0, d.length)];
+    console.log(q);
+     $.each(questions, function(i, quest) {
+      console.log(quest);
+      console.log(i);
+        if (q == quest) {
+            //set array
+           array = window["q" + i];
+           console.log(array);
         }
+
+      
+
     });
 
-    array[0][0] = 0;
-    d3.select("#mypiechart").remove();
 
-    console.log(array);
-    console.log(update1);
+    d3.select("#mypiechart").remove();
 
 
     svg = d3.select("body").selectAll("svg")
