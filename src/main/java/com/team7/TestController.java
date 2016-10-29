a001fd6117c0780150ab71131ac1a1aa5e3db14d
Subhrajit Debnath <sdebnath@jahnelgroup.com>
2016-10-28 20:04:44 -0400

    Fixed dependency issue

---
+++ src/main/java/com/team7/TestController.java
@@ -0,0 +1,15 @@
+package com.team7;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sd95 on 10/28/16.
+ */
+
+@Component
+@RestController
+public class TestController {
+
+}
