a5c236bd403377a582375014bb71dd8e3a93c842
Joonho Kim <idaho777@gmail.com>
2016-10-28 23:22:32 -0400

    drop schema

--- src/main/resources/db/migration/V1__first_tables.sql
+++ src/main/resources/db/migration/V1__first_tables.sql
@@ -1,11 +1,5 @@
-DROP SCHEMA IF EXISTS FTS_DB;
-CREATE SCHEMA IF NOT EXISTS FTS_DB;
-USE FTS_DB;
 
--- -----------------------------------------------------
--- Table `FTS_DB`.`survey`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FTS_DB`.`survey` (
+CREATE TABLE IF NOT EXISTS `survey` (
   `s_id`        INT NOT NULL AUTO_INCREMENT,
   `date`        date NOT NULL,
   `surveyor`    VARCHAR(45) NOT NULL,
@@ -14,9 +8,9 @@ CREATE TABLE IF NOT EXISTS `FTS_DB`.`survey` (
 
 
 -- -- -----------------------------------------------------
--- -- Table `FTS_DB`.`category`
+-- -- Table `category`
 -- -- -----------------------------------------------------
--- CREATE TABLE IF NOT EXISTS `FTS_DB`.`category` (
+-- CREATE TABLE IF NOT EXISTS `category` (
 --   `cat_id`        INT NOT NULL AUTO_INCREMENT,
 --   `answer_types`  VARCHAR(100) NOT NULL,
 --   PRIMARY KEY (`cat_id`),
@@ -24,9 +18,9 @@ CREATE TABLE IF NOT EXISTS `FTS_DB`.`survey` (
 
 
 -- -----------------------------------------------------
--- Table `FTS_DB`.`question`
+-- Table `question`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FTS_DB`.`question` (
+CREATE TABLE IF NOT EXISTS `question` (
   `q_id`            INT NOT NULL AUTO_INCREMENT,
   `test_question`   VARCHAR(100) NOT NULL,
   PRIMARY KEY (`q_id`),
@@ -34,9 +28,9 @@ CREATE TABLE IF NOT EXISTS `FTS_DB`.`question` (
 
 
 -- -- -----------------------------------------------------
--- -- Table `FTS_DB`.`question`
+-- -- Table `question`
 -- -- -----------------------------------------------------
--- CREATE TABLE IF NOT EXISTS `FTS_DB`.`question` (
+-- CREATE TABLE IF NOT EXISTS `question` (
 --   `q_id`            INT NOT NULL AUTO_INCREMENT,
 --   `test_question`   VARCHAR(100) NOT NULL,
 --   `cat_id`          INT NOT NULL,
@@ -44,26 +38,26 @@ CREATE TABLE IF NOT EXISTS `FTS_DB`.`question` (
 --   UNIQUE INDEX `question_UNIQUE` (`q_id` ASC),
 --   CONSTRAINT `cat_id`
 --     FOREIGN KEY (`cat_id`)
---     REFERENCES `FTS_DB`.`category` (`cat_id`)
+--     REFERENCES `category` (`cat_id`)
 --     ON DELETE NO ACTION
 --     ON UPDATE NO ACTION);
 
 
 
 -- -----------------------------------------------------
--- Table `FTS_DB`.`survey_question_answer`
+-- Table `survey_question_answer`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FTS_DB`.`survey_question_answer` (
+CREATE TABLE IF NOT EXISTS `survey_question_answer` (
   `s_id`    INT NOT NULL,
   `q_id`    INT NOT NULL,
   `test`    VARCHAR(100) NOT NULL,
   CONSTRAINT `s_id`
     FOREIGN KEY (`s_id`)
-    REFERENCES `FTS_DB`.`survey` (`s_id`)
+    REFERENCES `survey` (`s_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `q_id`
     FOREIGN KEY (`q_id`)
-    REFERENCES `FTS_DB`.`question` (`q_id`)
+    REFERENCES `question` (`q_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
