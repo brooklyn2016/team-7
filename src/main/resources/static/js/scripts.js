965cecddc0b90b8e37517e46f332252f170c8277
Neil <neil19962008@gmail.com>
2016-10-29 03:36:03 -0400

    Nav, Login, Survey, Dashboard
    -Added new elements to the navigation
    -Added login reroute
    -Added a new dashboard
    -Connected to backend

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -8,21 +8,23 @@ ftsapp.config(['$routeProvider', '$httpProvider', function($routeProvider, $http
 	$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 
     $routeProvider // route for the home page
-//    .when('/', {
-//        templateUrl: 'views/home.html',
-//        controller: 'mainController'
-//    })// route for the survey page
+
     .when('/', {
         templateUrl: 'views/home.html',
         controller: 'homecontroller'
-    })// route for the issues page
+    })// route for the survey page
     .when('/survey', {
         templateUrl: 'views/survey.html',
         controller: 'surveycontroller'
-    })// route for the charts page
+    })// route for the login page
     .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'logincontroller',
+    
+    })// route for the dashboard page
+    .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'dashboardcontroller',
     });
 }
 ]);
